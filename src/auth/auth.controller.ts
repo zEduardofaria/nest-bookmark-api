0508e1d3c74d5cbd5d3189889b26268b459fd3b1
Eduardo Faria <z.eduardofaria@gmail.com>
2022-05-22 21:22:20 -0300

    user signin

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -15,7 +15,10 @@ export class AuthController {
   }
 
   @Post('signin')
-  signin() {
-    return this.authService.signin();
+  signin(
+    @Body()
+    dto: AuthDto,
+  ) {
+    return this.authService.signin(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -33,7 +33,20 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return { message: 'Hello world, I am signed in!' };
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+
+    delete user.hash;
+    return user;
   }
 }

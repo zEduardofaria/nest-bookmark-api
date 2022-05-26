a8e445d4ea38c8fbf830f08d88c2713ec7c1fca8
Eduardo Faria <z.eduardofaria@gmail.com>
2022-05-26 18:32:07 -0300

    Creating testing structure

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,11 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import { PrismaService } from '../src/prisma/prisma.service';
 import { AppModule } from '../src/app.module';
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -17,11 +19,41 @@ describe('App e2e', () => {
       }),
     );
     await app.init();
+
+    prisma = app.get(PrismaService);
+
+    await prisma.cleanDb();
   });
 
   afterAll(() => {
     app.close();
   });
 
-  it.todo('should pass');
+  describe('Auth', () => {
+    describe('Signup', () => {
+      it.todo('sign up');
+    });
+
+    describe('Signin', () => {
+      it.todo('sign in');
+    });
+  });
+
+  describe('User', () => {
+    describe('Get Current User', () => {});
+
+    describe('Edit User', () => {});
+  });
+
+  describe('Bookmarks', () => {
+    describe('Create bookmark', () => {});
+
+    describe('Get Bookmarks', () => {});
+
+    describe('Get Bookmark by id', () => {});
+
+    describe('Edit Bookmark', () => {});
+
+    describe('Delete Bookmark', () => {});
+  });
 });
